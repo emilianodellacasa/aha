1244f000a88b9a3911d754fe13243c97136580b0
Emiliano Della Casa <emiliano.dellacasa@gmail.com>
2017-12-14 15:01:22 +0100

    Update main.py
    Bugfix

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ university_final_vote = int(sys.argv[1])
 
 def hire_or_not(vote):
   we_can_hire_him = False
-  if university_final_vote > 110:
+  if vote > 110:
     we_can_hire_him = True
   return we_can_hire_him
   
